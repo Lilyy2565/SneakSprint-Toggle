2e0071b079b7480ea3712e93a09b7e095f2d71d8
Lilyy2565 <lilys2565@gmail.com>
2025-07-13 12:41:22 +0100

    first commit

---
+++ src/client/java/com/soniczac7/sneaksprint/ConfigManager.java
@@ -0,0 +1,41 @@
+package com.soniczac7.sneaksprint;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ConfigManager {
+    // The config file should be stored in the Fabric config directory.
+    private static final File CONFIG_FILE = new File(FabricLoader.getInstance().getConfigDir().toFile(), "SneakSprintToggle.json");
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    public static SneakSprintToggleConfig config = new SneakSprintToggleConfig();
+
+    public static void loadConfig() {
+        if (CONFIG_FILE.exists()) {
+            try (FileReader reader = new FileReader(CONFIG_FILE)) {
+                config = GSON.fromJson(reader, SneakSprintToggleConfig.class);
+                System.out.println("Config loaded: " + config.toggleSprint + ", " + config.toggleSneak + ", " + config.textEnabled);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("No config file found. Saving default config.");
+            saveConfig();
+        }
+    }
+
+    public static void saveConfig() {
+        try (FileWriter writer = new FileWriter(CONFIG_FILE)) {
+            GSON.toJson(config, writer);
+            System.out.println("Config saved: " + config.toggleSprint + ", " + config.toggleSneak + ", " + config.textEnabled);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/client/java/com/soniczac7/sneaksprint/mixin/client/GameOptionsAccessor.java
@@ -0,0 +1,15 @@
+package com.soniczac7.sneaksprint.mixin.client;
+
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.SimpleOption;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(GameOptions.class)
+public interface GameOptionsAccessor {
+    @Accessor("sprintToggled")
+    SimpleOption<Boolean> getSprintToggled();
+
+    @Accessor("sneakToggled")
+    SimpleOption<Boolean> getSneakToggled();
+}
