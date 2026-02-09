7feeb0113db6a5a2589fcda9cb673bdfd3d26d78
Lilyy2565 <lilys2565@gmail.com>
2026-02-09 00:44:57 +0000

    Fix #1

--- src/client/java/com/lilyy2565/sneaksprint/mixin/client/DebugHudMixin.java
+++ src/client/java/com/lilyy2565/sneaksprint/mixin/client/DebugHudMixin.java
@@ -8,26 +8,19 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 //import org.spongepowered.asm.mixin.injection.Inject;
 //import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/*@Mixin(DebugHud.class)
-public class DebugHudMixin {
-
-    @Inject(method = "getLeftText", at = @At("RETURN"))
-    private void addSneakSprintInfo(CallbackInfoReturnable<List<String>> cir) {
-        List<String> debugInfo = cir.getReturnValue();
-        List<String> modInfo = SneakSprintToggleClient.getDebugInfo();
-        debugInfo.addAll(modInfo);
-    }
-}*/
 @Mixin(DebugHud.class)
 public class DebugHudMixin {
 
     @ModifyVariable(method = "method_51745", at = @At("HEAD"), ordinal = 0, argsOnly = true)
     private List<String> addSneakSprintInfo(List<String> debugInfo, net.minecraft.client.gui.DrawContext context, List<String> text, boolean left) {
         if (left) {
+            List<String> modifiedDebugInfo = new ArrayList<>(debugInfo);
             List<String> modInfo = SneakSprintToggleClient.getDebugInfo();
-            debugInfo.addAll(modInfo);
+            modifiedDebugInfo.addAll(modInfo);
+            return modifiedDebugInfo;
         }
         return debugInfo;
     }
